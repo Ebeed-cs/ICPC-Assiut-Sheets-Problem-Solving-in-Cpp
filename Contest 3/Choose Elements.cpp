4fc26489ddbd3645aa713bdee3e0e90ec9181e83
Ahmed Ebeed <aebeedcs@gmail.com>
2024-08-02 00:34:47 +0300

    ADD [Contest 3] - C

---
+++ Contest 3/Choose Elements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+ int N , K;
+ long long i = 0;
+ cin >> N >> K;
+ int A[N];
+ while(N--)
+ {
+  cin >> A[i++];
+ }
+ N = i;
+ i = 0;
+sort(A , A + N);
+while( K --)
+{
+  if(A[--N] > 0)
+    i += A[N];
+  else
+    break;
+}
+cout << i << endl;
+ return 0;
+}
