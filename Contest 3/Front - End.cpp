17b99abede3666cf50008ccac824cdd1e3ee3bcd
Ahmed Ebeed <aebeedcs@gmail.com>
2024-08-04 23:01:56 +0300

    ADD [Contest 3] - F

---
+++ Contest 3/Front - End.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int N , i = 0;
+  cin >> N;
+  int A[N];
+  while( N -- )
+  {
+    cin >> A[i++];
+  }
+  int HN;
+  N = i;
+  // HN = (N % 2 == 0)? N/2 : N/2 + 1 ;
+  HN = N /2;
+  i = 0;
+  while( i  < HN)
+  {
+    cout << A[i] << " ";
+      cout << A[N-1-i] << " ";
+    i++;
+  }
+  if (N % 2 != 0)
+  {
+    cout << A[N / 2];
+  }
+  return 0;
+}
