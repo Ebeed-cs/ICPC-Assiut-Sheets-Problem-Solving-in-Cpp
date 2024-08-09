14e4587ec6b6e65ae68c54128b2d919fba7098f8
Ahmed Ebeed <aebeedcs@gmail.com>
2024-08-09 20:32:12 +0300

    ADD [Sheet 4(Strings)] - Compare

---
+++ Sheet 4(Strings)/Compare.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+	string S1 , S2;
+	cin >> S1 >> S2;
+	int count = min(S1.size() , S2.size()) , i = 0;
+	string smaller = (S1.size() > S2.size())? S2 : S1;
+	while(count--)
+	{
+		if(int(S1[i]) < int(S2[i]))
+		{
+			smaller = S1;
+			break;
+		}			else if(int(S1[i]) > int(S2[i]))
+		{
+			smaller = S2;
+			break;
+		}
+		i++;
+	}
+	cout << smaller << endl;
+    return 0;
+}
