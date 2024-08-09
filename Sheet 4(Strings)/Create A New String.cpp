2ce5fa50879e32025e4687534fc44f03bf7d9283
Ahmed Ebeed <aebeedcs@gmail.com>
2024-08-09 18:55:58 +0300

    Init , ADD [Sheet 4(String)] - A

---
+++ Sheet 4(Strings)/Create A New String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string S1 , S2;
+  cin >> S1 >> S2;
+  cout << S1.size() << " " << S2.size() << endl;
+  cout << S1 << " " << S2 << endl;
+
+  return 0;
+}
+
